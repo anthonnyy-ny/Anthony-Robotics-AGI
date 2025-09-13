f5ab32707d8705ed8ac6f797e533a6380e8e92a0
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2025-09-13 20:58:59 +0800

    vault backup: 2025-09-13 20:58:59

--- demo.py
+++ demo.py
@@ -1,11 +1,1 @@
-# -*- coding: utf-8 -*-
-# 2^1 + 2^2 + ... + 2^n µÄ‚€Î»”µ
-n = int(input().strip())
 
-if n <= 0:
-    print(0)
-else:
-    # index = n % 4
-    # Œ¦‘ª‚€Î»”µžé: index=0 -> 0, 1->2, 2->6, 3->4
-    mapping = [0, 2, 6, 4]
-    print(mapping[n % 4])
