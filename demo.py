d263722b9a1f9ab20b58631cd24040221a4d82e0
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2025-09-22 19:36:25 +0800

    vault backup: 2025-09-22 19:36:25

--- demo.py
+++ demo.py
@@ -19,5 +19,5 @@ def digits_or_letters(s:str)->str:
 digits_or_letters("abc123")
 digits_or_letters("a1b2c3d")
 digits_or_letters("1a2b3c4")
-digits_or_letters("abc123")
-digits_or_letters("abc123")
+digits_or_letters("abc123!@#DEF")
+digits_or_letters("H3110 W0R1D")
