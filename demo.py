fa008174489694fda7167433f0fdbbc6f3e3709f
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2025-09-22 20:29:30 +0800

    vault backup: 2025-09-22 20:29:30

--- demo.py
+++ demo.py
@@ -1,24 +1,2 @@
 # -*- coding: utf-8 -*-
 
-def digits_or_letters(s:str)->str:
-    digits,letters=0,0
-
-    for c in s:
-        if c.isdigit():
-            digits+=1
-        elif c.isalpha():
-            letters+=1
-    
-    if digits>letters:
-        return "letters"
-    elif letters>digits:
-        return "digits"
-    else:
-        return "tie"
-
-print(digits_or_letters("abc123"))
-print(digits_or_letters("a1b2c3d"))
-print(digits_or_letters("1a2b3c4"))
-print(digits_or_letters("abc123!@#DEF"))
-print(digits_or_letters("H3110 W0R1D"))
-print(digits_or_letters("P455W0RD"))
