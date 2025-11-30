acf02315df7b02146ac8465ae472fa2868228d04
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2025-11-30 19:58:41 +0800

    vault backup: 2025-11-30 19:58:41

--- demo.py
+++ demo.py
@@ -1,19 +1,2 @@
 # -*- coding: utf-8 -*-
 
-def is_mirror(s1: str, s2: str) -> bool:
-    f1 = ""
-    for ch in s1:
-        if ch.isalpha():
-            f1 += ch
-    print(f1)
-
-    f2 = ""
-    for ch in s2:
-        if ch.isalpha():
-            f2 += ch
-    print(f2)
-    
-    return f1 == f2[::-1]
-
-print(is_mirror("abc", "cba"))              # True
-print(is_mirror(
